d9d200f7918e8c32b3961622d0e4e6ee594397e8
Rafael Nunez <rafael.rn.nunez@gmail.com>
2018-04-19 17:53:46 -0400

    Pass current state to client and display

--- src/app/containers/SurveyPage/component.js
+++ src/app/containers/SurveyPage/component.js
@@ -6,14 +6,16 @@ import TotalStats from "~/components/SurveyExample/TotalStats";
 import NoButton from "~/components/SurveyExample/NoButton";
 import YesButton from "~/components/SurveyExample/YesButton";
 
-const SurveyPage = ({ yes, no , total, addTotal, addYes, addNo, styles }) => (
-  <div className={styles.wrapper}>
-    <ImageExample />
-    <TotalStats yes={yes} no={no} total={total}/>
-    <YesButton onYesClick={addYes}/>
-    <NoButton onNoClick={addNo}/>
-  </div>
-);
+const SurveyPage = ({ yes, no, total, addYes, addNo, styles }) => {
+  return (
+    <div className={styles.wrapper}>
+      <ImageExample />
+      <TotalStats yes={yes} no={no} total={total} />
+      <YesButton onClick={addYes} />
+      <NoButton onClick={addNo} />
+    </div>
+  );
+};
 
 SurveyPage.propTypes = {
   styles: PropTypes.object.isRequired,
--- src/app/containers/SurveyPage/index.js
+++ src/app/containers/SurveyPage/index.js
@@ -1,24 +1,31 @@
-import { connect as connectFela } from "react-fela"
-import { connect as connectRedux } from "react-redux"
+import { connect as connectFela } from "react-fela";
+import { connect as connectRedux } from "react-redux";
 
-import * as styles from "./styles"
-import component from "./component"
-import { addYes, addNo, addTotal } from "../../store"
+import * as styles from "./styles";
+import component from "./component";
+import { surveyActions, surveySelectors } from "~/store";
 
-export const mapStateToProps = (state) => {
+export const mapStateToProps = state => {
+  const yes = surveySelectors.getYes(state);
+  const no = surveySelectors.getNo(state);
+  const total = surveySelectors.getTotal(state);
+
   return {
-    yes: state.yes,
-    no: state.no,
-    total: state.total
-  }
-}
+    yes,
+    no,
+    total
+  };
+};
 
-export const mapDispatchToProps = (state) => {
-  return {
-    addYes,
-    addNo
+export const mapDispatchToProps = dispatch => ({
+  addYes: () => {
+    dispatch(surveyActions.addYes());
+  },
+  addNo: () => {
+    dispatch(surveyActions.addNo());
   }
-}
+});
 
-export default connectRedux(mapStateToProps, mapDispatchToProps)(connectFela(styles)(component))
-// export default connectFela(styles)(component)
+export default connectRedux(mapStateToProps, mapDispatchToProps)(
+  connectFela(styles)(component)
+);
